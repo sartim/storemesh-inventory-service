2255958cf3075ce8ec3b719b74c697d399437de0
Tim <sarrtim@gmail.com>
2026-08-26 11:59:08 +0300

    feat: use PostgreSQL for deployed inventory (#2)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"os"
 
 	inventoryv1 "github.com/sartim/storemesh-inventory-service/gen/storemesh/inventory/v1"
+	"github.com/sartim/storemesh-inventory-service/internal/repository"
 	"github.com/sartim/storemesh-inventory-service/internal/service"
 	"google.golang.org/grpc"
 )
@@ -15,7 +18,16 @@ func main() {
 		log.Fatal(err)
 	}
 	server := grpc.NewServer()
-	inventoryv1.RegisterInventoryServiceServer(server, service.NewInventory())
+	if databaseURL := os.Getenv("DATABASE_URL"); databaseURL != "" {
+		store, err := repository.Open(context.Background(), databaseURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer store.Close()
+		inventoryv1.RegisterInventoryServiceServer(server, service.NewPersistentInventory(store))
+	} else {
+		inventoryv1.RegisterInventoryServiceServer(server, service.NewInventory())
+	}
 	log.Println("inventory service listening on :50051")
 	if err := server.Serve(listener); err != nil {
 		log.Fatal(err)
